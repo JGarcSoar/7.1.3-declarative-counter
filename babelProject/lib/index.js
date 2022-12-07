4ea71fd426ca018422cd6929f9d213766afcf22d
JGarcSoar <garciasoares.joe@gmail.com>
2022-12-06 21:49:45 -0800

    first commit

---
+++ babelProject/lib/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var multiply = function multiply(a, b) {
+  return a * b;
+};
+var firstNum = 5;
+var secondNum = 3;
+var product = multiply(firstNum, secondNum);
+console.log("The product of ".concat(firstNum, " and ").concat(secondNum, " is ").concat(product));
+var division = function division(a, b) {
+  return a / b;
+};
+var thirdNun = 10000;
+var fourthNum = 82;
+var divisionProduct = division(thirdNun, fourthNum);
+console.log("The product of ".concat(thirdNun, " and ").concat(fourthNum, " is ").concat(divisionProduct));
---
+++ babelProject/src/index.js
@@ -0,0 +1,13 @@
+const multiply = (a, b) => a * b;
+const firstNum = 5;
+const secondNum = 3;
+let product = multiply(firstNum, secondNum);
+console.log(`The product of ${firstNum} and ${secondNum} is ${product}`);
+
+const division = (a, b) => a / b;
+const thirdNun = 10000;
+const fourthNum = 82;
+let divisionProduct = division(thirdNun, fourthNum);
+console.log(
+  `The product of ${thirdNun} and ${fourthNum} is ${divisionProduct}`
+);
